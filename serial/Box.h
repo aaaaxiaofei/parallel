042986e83915d3c993ef71ac099876d331d6ad97
aaaaxiaofei <aaaaxiaofei@163.com>
2017-01-20 22:48:11 -0500

    Serial Code

---
+++ serial/Box.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <fstream>
+using namespace std;
+
+class CBox
+{
+public:
+	int boxID;
+	double left_x, right_x, upper_y, lower_y, width, height, T;
+	vector<int> left_id, right_id, top_id, bottom_id;
+
+	CBox(void);
+	~CBox(void);
+
+	void input(istream&);
+	void print();
+	double computeT(CBox box[]);
+};
+
